65949470de9e342c54f46d434e083003b0ca694c
Arya <aryaraikwar96@gmail.com>
2025-01-26 16:21:56 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  /*treat all the code as newer version of javascript*/
+
+// Null is a stand alone datatype but java is kinda not the Pisscaso programming language so it gives the out put a object
+
+// undefined is a stand alone datatype and its datatype is undefined only
+
+let a = 988
+let b = "Arya"
+let c = true
+let d = null
+let e = undefined
+
+console.log(typeof a)  //number
+console.log(typeof b)  //string
+console.log(typeof c)  //boolean
+
+console.log(typeof d)  //object (null is an object in javascript)
+console.log(typeof e)  //undefined
---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 733333
+let accountEmail = "Arya@gmail.com"
+var accountPassward = "123542"
+accountCity = "Indore"
+let accountState = "MadhyaPradesh"
+
+// accountId = 2
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState])
